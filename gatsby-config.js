dc12c8b0fbbdf1ae3dac03361e73f629302fcf58
JS <82275463+pinkelephant-blog@users.noreply.github.com>
2021-06-19 20:08:12 +0200

    add gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,17 @@ module.exports = {
     //   },
     // },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-VJXZL738Y7", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-feed`,
       options: {
         query: `
